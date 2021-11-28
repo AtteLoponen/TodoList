2a5ad6ebd4c991cc10f61a8c907edc8a2688c095
AtteLoponen <Atte.loponen@student.laurea.fi>
2021-11-28 17:25:11 +0200

    final

--- App.js
+++ App.js
@@ -1,105 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from 'react-native';
-import Tasks from './components/Tasks'
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
+import HomeScreen from "./screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
-
-  const [task, setTask] = useState();
-
-  const [taskItems, setTaskItem] = useState([]);
-
-  const addTask = () => {
-
-    setTaskItem([...taskItems, task])
-    setTask(null);
-  }
-
-  const deleteTask = (index) => {
-    let itemsCopy = [...taskItems];
-    itemsCopy.splice(index, 1);
-    setTaskItem(itemsCopy);
-  }
-
   return (
-    <View style={styles.container}>
-      <View style={styles.toDoHome}>
-        <Text style={styles.toDoTitle}> To-Do List  </Text>
-        <View style={styles.taskItem}>
-          {
-            taskItems.map((item, index) => {
-              return (
-                <TouchableOpacity key={index} onPress={() => deleteTask(index)}>
-                  <Tasks text={item} />
-                </TouchableOpacity>
-              )
-            })
-          }
-        </View>
-      </View>
-      <KeyboardAvoidingView style={styles.taskName}>
-        <TextInput style={styles.taskNameText} placeholder={'Write here'} value={task} onChangeText={text => setTask(text)} />
-        <TouchableOpacity onPress={() => addTask()}>
-          <View style={styles.addTask}>
-            <Image source={{ uri: 'https://www.iconsdb.com/icons/preview/black/plus-xxl.png' }} style={styles.addTaskText}></Image>
-          </View>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator >
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen
+        options={{ headerShown: false }} 
+        name="Home" 
+        component={HomeScreen} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#FFF7D1',
+    
   },
-  toDoHome: {
-    paddingTop: 90,
-    paddingHorizontal: 30,
-  },
-  toDoTitle: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    marginBottom: 40,
-  },
-  taskItem: {
-
-  },
-  taskName: {
-    position: 'absolute',
-    bottom: 60,
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-
-
-  },
-  taskNameText: {
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-    width: 250,
-    borderRadius: 60,
-    backgroundColor: '#FFF2AB',
-    borderWidth: 1,
-
-  },
-  addTask: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#FFF2AB',
-    borderWidth: 1,
-    borderRadius: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-
-
-  },
-  addTaskText: {
-    height: 30,
-    width: 30,
-
-  },
-
+  navi: {
+    backgroundColor: "#FFF7D1", 
+  }
 });
